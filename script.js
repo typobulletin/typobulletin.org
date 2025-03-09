16dd5d744d31e7524ede3c6540bb1372c0e8d52c
Max Ilinov <ilimaksim@gmail.com>
2025-03-09 05:30:43 +0300

    fix path

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function setRandomImageSrc() {
     const randomNumber = Math.floor(Math.random() * 216) + 1;
     const paddedNumber = String(randomNumber).padStart(3, '0');
     const imgElement = document.getElementById('randomLetter');
-    imgElement.src = `jpg/image-${paddedNumber}.jpg`;
+    imgElement.src = `../jpg/image-${paddedNumber}.jpg`;
   }
   
   window.onload = setRandomImageSrc;
